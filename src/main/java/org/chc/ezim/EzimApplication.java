461182e4b912b9dd7c29b27107cb262189097682
chihaicheng <chc2075983104@163.com>
2024-05-10 22:06:25 +0800

    init: 项目初始化

---
+++ src/main/java/org/chc/ezim/EzimApplication.java
@@ -0,0 +1,13 @@
+package org.chc.ezim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chc/ezim/EzimApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chc.ezim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
